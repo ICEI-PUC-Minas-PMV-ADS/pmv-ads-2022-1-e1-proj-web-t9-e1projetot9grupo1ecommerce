dca0a3f261832a3dc89c2bd79a2a016037fbe1b1
Gabriel Puddo <suporte@regexsolutions.com.br>
2022-05-30 23:21:03 -0300

    https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t9-e1projetot9grupo1ecommerce/issues/48 concluindo pagina detalhes

--- src/js/detalhes-produto.js
+++ src/js/detalhes-produto.js
@@ -4,22 +4,91 @@ $(document).ready(event => {
 
 let produto;
 
+let tamanhoSelecionado;
+let corSelecionada;
+
 function carregarDetalhesProduto() {
     
     var url = new URL(window.location.href);
     var idProduto = url.searchParams.get("produto");
     
     produto = listaProdutos.find(p => p.id == idProduto)
-    console.log(produto)
+
+    document.getElementById('descricao-produto').textContent = produto['descricao'];
+    document.getElementById('preco-produto').textContent = `R$ ${(produto['preco'] || 0).toFixed(2).replace('.', ',')}`;
+
+    for(let tamanho of produto.tamanhos) {
+
+        let html = `
+            <li>
+                <div onclick="changeTamanho('${tamanho}')">${tamanho}</div>
+            </li>
+        `;
+
+        $('.lista-tamanhos').append(html);
+
+    }
+
+    for(let cor of produto.cores) {
+
+        let html = `
+            <li title="${cor.titulo}">
+                <div onclick="changeCor('${cor.hexadecimal}', '${cor.titulo}')" style="background-color: ${cor.hexadecimal};"></div>
+            </li>
+        `;
+
+        $('.lista-cores').append(html);
+
+    }
 
 }
 
 async function adicionarCarrinho() {
+    
+    if(!produto.tamanhoSelecionado) {
+        alert('Selecione um tamanho!');
+        return;
+    }
+
+    if(!produto.corSelecionada) {
+        alert('Selecione uma cor!');
+        return;
+    }
+
 
     let carrinho = await getCarrinho();
 
     carrinho.produtos.push(produto);
 
     await saveCarrinho(carrinho);
+    
+    limpaCorTamanhoSelecionados();
+
+}
+
+function changeCor(cor, textoCor) {
+
+    produto.corSelecionada = cor;
+
+    document.getElementById('texto-cor').textContent = textoCor;
+
+}
+
+function changeTamanho(tamanho) {
+
+    produto.tamanhoSelecionado = tamanho;
+
+    document.getElementById('texto-tamanho').textContent = tamanho;
+
+}
+
+function limpaCorTamanhoSelecionados() {
+
+    delete produto.corSelecionada;
+    document.getElementById('texto-cor').textContent = '';
+    
+
+    delete produto.tamanhoSelecionado;
+    document.getElementById('texto-tamanho').textContent = '';
 
 }
