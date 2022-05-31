0e81b1e706e4a89dc821390b6aa8988450214d3b
brunofs12 <zkatan4@gmail.com>
2022-05-31 13:02:50 -0300

    Cookie popup JS

--- src/js/header-footer.js
+++ src/js/header-footer.js
@@ -6,6 +6,27 @@ $(document).ready(event => {
 
 }) 
 
+
+
+/* Popup Cookie
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
+
+
+
+
 /*     
     // Função de animação usando jquery
 
