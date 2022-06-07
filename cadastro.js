122a24aed61b4f217585bc477c0739ca9dcfb02a
brenol-gar <bgarrido@sga.pucminas.br>
2022-06-07 00:50:47 -0300

    olhoCadastro

---
+++ cadastro.js
@@ -0,0 +1,24 @@
+function eyeClick() {
+    let passwordInput = document.querySelectorAll(.password)
+    let inputTypeIsPassword = passwordInput[0].type == password
+
+
+    function showPassword() {
+        passwordInput[0].setAttribute(type, text)
+        passwordInput[1].setAttribute(type, text)
+        
+    }
+    
+    function hidePassword() {
+        passwordInput[0].setAttribute(type, password)
+        passwordInput[1].setAttribute(type, password)
+       
+    }
+    if (inputTypeIsPassword) {
+        showPassword()
+
+    } else {
+        hidePassword()
+
+    }
+}
