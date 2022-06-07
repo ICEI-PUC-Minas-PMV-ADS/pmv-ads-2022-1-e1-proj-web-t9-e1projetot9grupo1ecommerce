0d39560e1c4c00fbaff3090c4aa50d335b35e558
brenol-gar <bgarrido@sga.pucminas.br>
2022-06-07 13:55:59 -0300

    java cadastro corrigido

--- cadastro.js
+++ cadastro.js
@@ -1,18 +1,18 @@
-
 function eyeClick() {
-    let passwordInput = document.querySelectorAll(.password)
-    let inputTypeIsPassword = passwordInput[0].type == password
+    let passwordInput = document.querySelectorAll('.password')
+    let inputTypeIsPassword = passwordInput[0].type == "password"
+    
 
 
     function showPassword() {
-        passwordInput[0].setAttribute(type, text)
-        passwordInput[1].setAttribute(type, text)
+        passwordInput[0].setAttribute('type', 'text')
+        passwordInput[1].setAttribute('type', 'text')
         
     }
     
     function hidePassword() {
-        passwordInput[0].setAttribute(type, password)
-        passwordInput[1].setAttribute(type, password)
+        passwordInput[0].setAttribute('type', "password")
+        passwordInput[1].setAttribute('type', 'password')
        
     }
     if (inputTypeIsPassword) {
